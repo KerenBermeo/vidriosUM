c3219f8ef73fde5740791b8005969ed276b544e5
KerenBermeo <bermeokeren@gmail.com>
2025-04-13 14:19:53 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./index.html", // Si estás usando Vite o necesitas escanear el HTML principal
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
